c0c1410f178d093fed647ed38fc04a00ed12b1fb
Red-Rim <salmaouakhir20@gmail.com>
2024-03-08 01:28:40 +0100

    make_classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-    const roomSizes = [19, 20, 34];
-    const rooms = roomSizes.map(size => new ClassRoom(size));
-    return rooms;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
 
 export default initializeRooms;
