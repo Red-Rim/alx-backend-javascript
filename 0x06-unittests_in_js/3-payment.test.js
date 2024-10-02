7e15634952f049ab5cf2bfd1f321e4f9897a76f6
Red-Rim <salmaouakhir20@gmail.com>
2024-10-02 22:31:20 +0100

    .

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,28 +1,16 @@
 
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  let spy;
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-  beforeEach(function() {
-    spy = sinon.spy(console, 'log');
-  });
-
-  afterEach(function() {
-    spy.restore();
-  });
-
-  it('should log the correct total for 100 and 20', function() {
     sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledOnce(spy);
-    sinon.assert.calledWith(spy, 'The total is: 120');
-  });
-
-  it('should log the correct total for 10 and 10', function() {
-    sendPaymentRequestToApi(10, 10);
-    sinon.assert.calledOnce(spy);
-    sinon.assert.calledWith(spy, 'The total is: 20');
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
